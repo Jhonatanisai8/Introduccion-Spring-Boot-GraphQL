7ee8bf0e15c92598a09598d9419fc7096f6007a5
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-22 21:46:11 -0500

    init

---
+++ src/main/java/com/isai/demo_api_graphql/DemoApiGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_api_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_api_graphql/DemoApiGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_api_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
